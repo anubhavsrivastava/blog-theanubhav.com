b7d024c32aa991d2a04526f3b5184613baa7a534
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-05 01:10:09 +0530

    updated information

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,32 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
+  siteTitle: "theAnubhav - Anubhav's Personal blog", // <title>
+  shortSiteTitle: "theAnubhav - Anubhav's Personal blog", // <title> ending for posts and pages
+  siteDescription: "theAnubhav - Anubhav's Personal blog",
   siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Anubhav Srivastava",
+  authorTwitterAccount: "onlyanubhav",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Anubhav Srivastava",
+  infoTitleNote: "Personal blog",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestName: "theAnubhav - Anubhav's Personal blog",
   manifestShortName: "PersonalBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "anubhav.srivastava00@gmail.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/anubhavsrivastava" },
+    { name: "twitter", url: "https://twitter.com/onlyanubhav" },
+    { name: "facebook", url: "https://facebook.com/theanubhav" }
   ]
 };
